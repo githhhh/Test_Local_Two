875d0d149aee301dce46ccd6da159e3573acbb6d
tangbin <tangbineml@163.com>
2014-08-14 15:12:02 +0800

    自动跟新sb，发布时须去掉base

--- Debug_St.sh
+++ Debug_St.sh
@@ -18,18 +18,13 @@ baseLprojName="Base.lproj"
 # Find storyboard file full path inside project folder
 for storyboardPath in `find ${SRCROOT} -name "*$storyboardExt" -print`
 do
-echo "============storyboardPath======================="
-echo $storyboardPath
 # Get storyboard file name and folder
 storyboardFile=$(basename "$storyboardPath")
 storyboardDir=$(dirname "$storyboardPath")
 # Get storyboardDir upLevel dirName  and dir
 currentSbLprojName=$(basename "$storyboardDir")
 currentSbLprojDir=$(dirname "$storyboardDir")
-echo "============currentSbLprojName======================="
-echo $currentSbLprojName
-echo "============currentSbLprojDir======================="
-echo $currentSbLprojDir
+
 #limite on base.lproj
 if [ "$currentSbLprojName" != "$baseLprojName" ]; then
 echo "==========storyboardDir=============="
@@ -45,13 +40,10 @@ stringsFile=$(basename "$baseStringsPath")
 ibtool --export-strings-file $newBaseStringsPath $storyboardPath
 iconv -f UTF-16 -t UTF-8 $newBaseStringsPath > $baseStringsPath
 rm $newBaseStringsPath
-echo "==========for other lproj====================="
 #for  on base.lproj folder and find other language.lproj dir
 for localeStringsDir in `find ${currentSbLprojDir} -name "*$localeDirExt" -print`
 do
 otherLprojName=$(basename "$localeStringsDir")
-echo "==========otherLprojName====================="
-echo $otherLprojName
 # Skip Base.lproj folder
 if [ "$otherLprojName" != "$baseLprojName" ];then
 localeStringsPath=$localeStringsDir/$stringsFile
@@ -61,19 +53,16 @@ if [ ! -e $localeStringsPath ]||[ ! -s $localeStringsPath ]; then
 cp $baseStringsPath $localeStringsPath
 else
 oldLocaleStringsPath=$(echo "$localeStringsPath" | sed "s/$stringsExt/$oldStringsExt/")
-echo "==========oldLocaleStringsPath====================="
-echo $oldLocaleStringsPath
 cp $localeStringsPath $oldLocaleStringsPath
-
 echo "==========Merge====================="
 # Merge baseStringsPath to localeStringsPath
 awk  'NR == FNR && /^\/\*/ {x=$0; getline; a[x]=$0; next} /^\/\*/ {x=$0; print; getline; $0=a[x]?a[x]:$0; printf $0"\n\n"}' $oldLocaleStringsPath $baseStringsPath > $localeStringsPath
 rm $oldLocaleStringsPath
 #on debug convert .string to .storyboard
-if [ -e $localeStoryBoardPath];then
-rm $localeStoryBoardPath
-fi
-ibtool --strings-file $localeStringsPath --write $localeStoryBoardPath $storyboardPath
+#if [ -e $localeStoryBoardPath];then
+#rm $localeStoryBoardPath
+#fi
+#ibtool --strings-file $localeStringsPath --write $localeStoryBoardPath $storyboardPath
 fi
 fi
 done
@@ -81,7 +70,7 @@ done
 #remove baseString
 rm $baseStringsPath
 #debug
-if find $storyboardPath -prune -newer $ll -print | grep -q .; then
-echo "=================="
-fi
+#if find $storyboardPath -prune -newer $ll -print | grep -q .; then
+#echo "=================="
+#fi
 done
